9dbae38fc40e3f3267f861ce21df57735512c353
Erestrepo05 <e05072003@gmail.com>
2022-05-29 21:55:50 -0500

    se termino y se inicio
    se termina la del video y empieza la propia a solas

--- calculadora ejemplo/index.js
+++ calculadora ejemplo/index.js
@@ -5,12 +5,10 @@ const botonesOperadores = document.querySelectorAll('.operador');
 
 const display = new Display(displayValorAnterior, displayValorActual);
 
-
 botonesNumeros.forEach(boton => {
     boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
 });
 
-botonesOperadores.forEach(botones => {
+botonesOperadores.forEach(boton => {
     boton.addEventListener('click', () => display.computar(boton.value))
 });
-
